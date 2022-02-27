0a5d45fd04db1ad8054bbb044dd323677b3029e4
yona78 <87584566+yona78@users.noreply.github.com>
2022-02-27 17:29:55 +0200

    Update Ray.java

--- Primitives/Ray.java
+++ Primitives/Ray.java
@@ -1,5 +1,31 @@
-package Primitives;
+package primitives;
 
 public class Ray {
-    
+    Point p0;
+    Vector dir;
+    public
+    Ray(Point p, Vector vec)
+    {
+    	p0 = p;
+    	dir = vec.normalize();
+    }
+    public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Point))
+			return false;
+		Ray other = (Ray) obj;
+		return this.p0.equals(other.p0) && this.dir.equals(other.dir);
+	}
+    public String toString() {
+    	return "Point:" + this.p0.toString() + " Vector:" + this.dir.toString();
+    }
+    Point get_po() {
+    	return p0;
+    }
+    Vector get_dir() {
+    	return dir;
+    }
 }
