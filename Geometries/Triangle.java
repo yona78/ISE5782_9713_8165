8421bfa8d6e7567a57f22f5110e72c994613f385
הילל קרויטורו <1hilell2@gmail.com>
2022-02-27 15:02:03 +0200

    comments and constructor

--- Geometries/Triangle.java
+++ Geometries/Triangle.java
@@ -1,6 +1,24 @@
+/**
+ * 
+ */
 package Geometries;
 import Primitives.Point;
+/**
+ * This class present a triangle
+ * 
+ * @author Hillel Kroitoro
+ */
 
 public class Triangle extends Polygon{
-    public Triangle(Point)
+    /**
+	 * Constructor to initialize Triangle based Poligon with its three points
+	 * 
+	 * @param p1 first point.
+	 * @param p2 second point.
+	 * @param p3 third point.
+	 */
+    public Triangle(Point p1, Point p2, Point p3)
+    {
+        super(p1, p2, p3);
+    }
 }
