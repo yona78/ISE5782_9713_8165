ab512faa8a02b7a55747b857b93a8a4226b3478c
הילל קרויטורו <1hilell2@gmail.com>
2022-02-27 15:47:21 +0200

    Cylinder

---
+++ Geometries/Cylinder.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package Geometries;
+/**
+ * This class present a cylinder.
+ * 
+ * @author Hillel Kroitoro
+ */
+
+import Primitives.Ray;
+
+public class Cylinder extends Tube {
+    //#region Fields
+    Ray ray;
+    double radius;
+    double high;
+    //#endregion
+
+    /**
+	 * Constructor to initialize sphere with its ray and radius.
+	 * 
+	 * @param ray the center of the base of the sircle base.
+	 * @param radius the radius of the sircle base.
+	 */
+	public Cylinder(Ray ray, Double radius, double high) {
+		super(ray, radius);
+        this.high = high;
+	}
+    
+}
