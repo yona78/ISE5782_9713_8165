b3866b5cfd79da4ef11185d1e66e36a61d1b51fd
yona78 <87584566+yona78@users.noreply.github.com>
2022-02-27 23:35:42 +0200

    Update Ray.java

--- Primitives/Ray.java
+++ Primitives/Ray.java
@@ -1,5 +1,4 @@
 package primitives;
-
 /**
  * This class will serve all primitive classes based on three numbers
  * 
@@ -8,21 +7,20 @@ package primitives;
 public class Ray {
 	final Point p0;
 	final Vector dir;
-
-	public
-	/**
-	 * Constructor to initialize Ray based object with its Point value and vector
+    public
+    /**
+	 * Constructor to initialize Ray based object with its Point value and vector 
 	 * 
 	 * @param p first number value
-	 *          @para, vec secound object to initialize
+	 * @para, vec secound object to initialize
 	 */
-	Ray(Point p, Vector vec) {
-		p0 = p;
-		dir = vec.normalize();
-	}
-
-	@Override
-	public boolean equals(Object obj) {
+    Ray(Point p, Vector vec)
+    {
+    	p0 = p;
+    	dir = vec.normalize();
+    }
+    @Override
+    public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -32,27 +30,24 @@ public class Ray {
 		Ray other = (Ray) obj;
 		return this.p0.equals(other.p0) && this.dir.equals(other.dir);
 	}
-
-	@Override
-	public String toString() {
-		return "Point:" + this.p0.toString() + " Vector:" + this.dir.toString();
-	}
-
-	/**
-	 * get the point of the ray
+    @Override
+    public String toString() {
+    	return "(" + this.p0.toString() +  this.dir.toString() + " )" ;
+    }
+    /**
+	 *get the point of the ray
 	 *
 	 * @return field p0
 	 */
-	Point get_po() {
-		return p0;
-	}
-
-	/**
-	 * get the vector of the ray
+    public Point get_po() {
+    	return p0;
+    }
+    /**
+	 *get the vector of the ray
 	 *
 	 * @return field dir
 	 */
-	Vector get_dir() {
-		return dir;
-	}
+    public Vector get_dir() {
+    	return dir;
+    }
 }
