fe730891e49358c3d15cb7ca54de68de51807a59
הילל קרויטורו <1hilell2@gmail.com>
2022-02-27 17:43:08 +0200

    Merge

--- Primitives/Vector.java
+++ Primitives/Vector.java
@@ -1,79 +1,88 @@
-package primitives;
+package Primitives;
 
+import java.lang.Math;
 
 public class Vector extends Point {
-	public
-    Vector(double x, double y,double z) {
-    	super(x,y,z);
+
+    public Vector(double x, double y, double z) {
+        super(x, y, z);
         if (this.xyz.equals(Double3.ZERO)) {
-        	throw new IllegalArgumentException("cant create zero vector");
+            throw new IllegalArgumentException("cant create zero vector");
         }
     }
-	Vector(Double3 p) {
-    	super(p);
+
+    Vector(Double3 xyz) {
+        super(xyz);
         if (this.xyz.equals(Double3.ZERO)) {
-        	throw new IllegalArgumentException("cant create zero vector");
+            throw new IllegalArgumentException();
         }
     }
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Point))
-			return false;
-		Point other = (Point) obj;
-		return super.equals(other);
-	}
+
+    public Vector(Point p1, Point p2) {
+        super(p2.xyz.d1 - p1.xyz.d1, p2.xyz.d2 - p1.xyz.d2, p2.xyz.d3 - p1.xyz.d3);
+        /*
+         * double x = p2.xyz.d1 - p1.xyz.d1;
+         * double y = p2.xyz.d2 - p1.xyz.d2;
+         * double z = p2.xyz.d3 - p1.xyz.d3;
+         * super(x,y,z);
+         */
+        if (this.xyz.equals(Double3.ZERO)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Point))
+            return false;
+        Point other = (Point) obj;
+        return super.equals(other);
+    }
+
     public String toString() {
-    	return "Vector:" + super.toString();
+        return "Vector:" + super.toString();
     }
-    Double3 get_point() {
-    	return xyz;
+
+    public Double3 getPoint() {
+        return xyz;
     }
-    
-    Vector add (Vector vec) {
-    	Point other = this.add(vec);
-    	return new Vector(other.xyz.d1, other.xyz.d2, other.xyz.d3);
+
+    public Vector Normalize() {
+        double length = Length();
+        return new Vector(xyz.d1 / length, xyz.d2 / length, xyz.d3 / length);
     }
-    
-    Vector subtract (Vector vec) {
-    	return this.subtract(vec);
+
+    public double Length() {
+        return Math.sqrt(LengthSquared());
     }
-    
-    Vector scale (double scale) {
-    	Double3 other = this.xyz.scale(scale);
-    	return new Vector(other.d1, other.d2, other.d3);
+
+    public double LengthSquared() {
+        return xyz.d1 * xyz.d1 + xyz.d2 * xyz.d2 + xyz.d3 * xyz.d3;
     }
-    
-    double dotProduct(Vector vec) {
-    	double x = (this.xyz.d1 * vec.xyz.d1);
-    	double y = (this.xyz.d2 * vec.xyz.d2) ;
-    	double z = (this.xyz.d3 * vec.xyz.d3);
-    	return x + y + z;
+
+    public Vector CrossProduct(Vector vector) {
+        double x = this.xyz.d1;
+        double y = this.xyz.d2;
+        double z = this.xyz.d3;
+        double x1 = vector.xyz.d1;
+        double y1 = vector.xyz.d2;
+        double z1 = vector.xyz.d3;
+        return new Vector(y * z1 - z * y1, z * x1 - x * z1, x * y1 - y * x1);
     }
-    
-    Vector crossProduct(Vector vec) {
-    	double x = (this.xyz.d2 * vec.xyz.d3) - (this.xyz.d3 * vec.xyz.d2);
-    	double y = (this.xyz.d3 * vec.xyz.d1) - (this.xyz.d1 * vec.xyz.d3);
-    	double z = (this.xyz.d1 * vec.xyz.d2) - (this.xyz.d2 * vec.xyz.d1);
-    	return new Vector(x, y, z);
+
+    public Vector scale(double scale) {
+        Double3 other = this.xyz.scale(scale);
+        return new Vector(other.d1, other.d2, other.d3);
     }
-    
-    double lengthSquared() {
-    	double x = (this.xyz.d1) * (this.xyz.d1);
-    	double y = (this.xyz.d2) * (this.xyz.d2);
-    	double z = (this.xyz.d3) * (this.xyz.d3);
-    	return  x + y +z;
+
+    public double dotProduct(Vector vec) {
+        double x = (this.xyz.d1 * vec.xyz.d1);
+        double y = (this.xyz.d2 * vec.xyz.d2);
+        double z = (this.xyz.d3 * vec.xyz.d3);
+        return x + y + z;
     }
-    
-   double length() {
-	   return Math.sqrt(this.lengthSquared());
-   }
-   
-   Vector normalize() {
-	   double len = this.length();
-	   Vector other = this.scale((1/len));
-	   return other;
-   }
 }
