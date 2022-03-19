d120e606e8f2b7b5a24150dbdb2bd6808af91852
yona78 <yonaorunov1@gmail.com>
2022-03-19 23:31:11 +0200

    h

--- src/unittests/geometries/TriangleTests.java
+++ src/unittests/geometries/TriangleTests.java
@@ -35,47 +35,39 @@ class TriangleTests {
 	public void testFindIntsersections() {
 		Triangle tr = new Triangle(new Point(0, 0, 0), new Point(1, 0, 0), new Point(0, 1, 0));
 		Vector v = new Vector(0, 0, 1);
-		List<Point> emptyList = null;
 		// ============ Equivalence Partitions Tests ==============
 		
 		// TC01: There is a simple single test here
-		assertEquals(new Point(0, 0, 1), tr.findIntsersections(new Ray(new Point(0.25, 0.25, -1), v)).get(0), "Bad Point to trinagle");//checks on the triangle 
+		assertEquals(new Point(0.25, 0.25, -1), tr.findIntsersections(new Ray(new Point(0.25, 0.25, -1), v)).get(0), "Bad Point to trinagle");//checks on the triangle 
 		
 		// =============== Boundary Values Tests ==================
 		
 		// TC11: Test when the point is out of the triangle(side way)
-		assertEquals(emptyList, //
-				() -> tr.findIntsersections(new Ray(new Point(-1, 0.5, -1), v)), //
+		assertNull(tr.findIntsersections(new Ray(new Point(-1, 0.5, -1), v)), //
 				"Test when the point is out of triangle (side way) faild");
 		
 		// TC12: Test when the point is out of the triangle(angle way)
-		assertEquals(emptyList, //
-				() -> tr.findIntsersections(new Ray(new Point(-1, -1, -1), v)), //
+		assertNull(tr.findIntsersections(new Ray(new Point(-1, -1, -1), v)), //
 				"Test when the point is out of triangle (angle way) faild");	
 		
 		// TC13: Test when the ray's point is on the triangle
-		assertEquals(emptyList, //
-				() -> tr.findIntsersections(new Ray(new Point(0.25, 0.25, 0), v)), //
+		assertNull(tr.findIntsersections(new Ray(new Point(0.25, 0.25, 0), v)), //
 				"Test when the ray is on the triangle faild");
 		
 		// TC14: Test when the ray's point is above the triangle
-		assertEquals(emptyList, //
-				() -> tr.findIntsersections(new Ray(new Point(0.25, 0.25, 1), v)), //
+		assertNull(tr.findIntsersections(new Ray(new Point(0.25, 0.25, 1), v)), //
 				"Test when the ray is above the triangle faild");
 
 		// TC15: Test when the point is in the edge of the triangle
-		assertEquals(emptyList, //
-				() -> tr.findIntsersections(new Ray(new Point(0, 0.5, 1), v)), //
+		assertNull(tr.findIntsersections(new Ray(new Point(0, 0.5, 1), v)), //
 				"Test when the point is in the edge of the triangle faild");
 		
 		// TC16: Test when the point is in the angle of the triangle
-		assertEquals(emptyList, //
-				() -> tr.findIntsersections(new Ray(new Point(0, 0, 1), v)), //
+		assertNull(tr.findIntsersections(new Ray(new Point(0, 0, 1), v)), //
 				"Test when the point is in the angle of the triangle faild");
 		
 		// TC17: Test when the point is on edge's continuation
-		assertEquals(emptyList, //
-				() -> tr.findIntsersections(new Ray(new Point(0, -1, 1), v)), //
+		assertNull(tr.findIntsersections(new Ray(new Point(0, -1, 1), v)), //
 				"Test when the point is on edge's continuation faild");
 	}
 }
