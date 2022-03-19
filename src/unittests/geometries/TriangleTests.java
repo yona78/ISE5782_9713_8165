9850b0df2b402fb1a6e586b9507116788b3073fc
yona78 <yonaorunov1@gmail.com>
2022-03-19 23:45:24 +0200

    TARGIL3

--- src/unittests/geometries/TriangleTests.java
+++ src/unittests/geometries/TriangleTests.java
@@ -35,33 +35,30 @@ class TriangleTests {
 	public void testFindIntsersections() {
 		Triangle tr = new Triangle(new Point(0, 0, 0), new Point(1, 0, 0), new Point(0, 1, 0));
 		Vector v = new Vector(0, 0, 1);
-		
-		//EP
 		// ============ Equivalence Partitions Tests ==============
 		
 		// TC01: There is a simple single test here
 		assertEquals(new Point(0.25, 0.25, 0), tr.findIntsersections(new Ray(new Point(0.25, 0.25, -1), v)).get(0), "Bad Point to trinagle");//checks on the triangle 
 		
-		// =============== Boundary Values Tests ==================
 		
-		// TC11: Test when the point is out of the triangle(against edge)
+		// TC02: Test when the point is out of the triangle(against edge)
 		assertNull(tr.findIntsersections(new Ray(new Point(-1, 0.5, -1), v)), //
 				"Test when the point is out of triangle (against edge) failed");
 		
-		// TC12: Test when the point is out of the triangle(against vertex)
+		// TC03: Test when the point is out of the triangle(against vertex)
 		assertNull(tr.findIntsersections(new Ray(new Point(-1, -1, -1), v)), //
 				"Test when the point is out of triangle (against vertex) failed");	
 		
-		//BVA
-		// TC13: Test when the point is in the edge of the triangle
+		// =============== Boundary Values Tests ==================
+		// TC11: Test when the point is in the edge of the triangle
 		assertNull(tr.findIntsersections(new Ray(new Point(0, 0.5, 1), v)), //
 				"Test when the point is in the edge of the triangle failed");
 		
-		// TC14: Test when the point is in the angle of the triangle
+		// TC12: Test when the point is in the angle of the triangle
 		assertNull(tr.findIntsersections(new Ray(new Point(0, 0, 1), v)), //
 				"Test when the point is in the angle of the triangle failed");
 		
-		// TC15: Test when the point is on edge's continuation
+		// TC13: Test when the point is on edge's continuation
 		assertNull(tr.findIntsersections(new Ray(new Point(0, -1, 1), v)), //
 				"Test when the point is on edge's continuation failed");
 		
