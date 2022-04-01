a3d7ce5572ab670a1eded7d67a8b258cbaa392e2
הילל קרויטורו <1hilell2@gmail.com>
2022-04-01 15:14:04 +0300

    6.B.1

---
+++ src/lighting/Light.java
@@ -0,0 +1,30 @@
+package lighting;
+
+import primitives.*;
+
+/**
+ * This class is the base of the light types
+ * 
+ * @author Hillel Kroitoro, Yona Orunov
+ */
+abstract class Light {
+	private Color intensity;
+	
+	/**
+	 * Constructor to initialize Light based on color
+	 * 
+	 * @param intensity is the intensity of the light
+	 */
+	protected Light(Color intensity) {
+		this.intensity = intensity;
+	}
+	
+	/**
+	 * Getter of the intensity of the light
+	 *
+	 * @return field intensity
+	 */
+	public Color getIntensity() {
+		return intensity;
+	}
+}
