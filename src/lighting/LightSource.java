4a329f48cf58a0ce191eddd38ad202c5a66fac8f
הילל קרויטורו <1hilell2@gmail.com>
2022-04-01 15:34:09 +0300

    s

--- src/lighting/LightSource.java
+++ src/lighting/LightSource.java
@@ -1,5 +1,25 @@
 package lighting;
 
+import primitives.*;
+
+/**
+ * This interface is the base of the light sources
+ * 
+ * @author Hillel Kroitoro, Yona Orunov
+ */
 public interface LightSource {
+	/**
+	 * 
+	 * 
+	 * @param p is the point
+	 * @return the intensity
+	 */
+	public Color getIntensity(Point p);
+
+	/**
+	 * 
+	 * @param p is the point
+	 */
+	public Vector getL(Point p);
 
 }
