101f0145ad67daa8e51da02d57b2cc68f0b0f95f
הילל קרויטורו <1hilell2@gmail.com>
2022-04-03 08:01:33 +0300

    Material class

---
+++ src/primitives/Material.java
@@ -0,0 +1,13 @@
+package primitives;
+
+/**
+ * This class used for the bodies' material effects
+ * 
+ * @author Yona Orunov, Hillel Kroitoro
+ */
+
+public class Material {
+	public Double3 kD;
+	public Double3 kS;
+	public int nShininess;
+}
