9631c7a6451b7fc24670f24ca72751f32392f9d7
הילל קרויטורו <1hilell2@gmail.com>
2022-04-08 12:19:26 +0300

    range to spotlight

--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -8,18 +8,33 @@ import primitives.*;
  * @author Hillel Kroitoro, Yona Orunov
  */
 public class SpotLight extends PointLight{
-	private  Vector direction;
-	
+	private Vector direction;
+	private double range;
 	/**
 	 * Constructor to initialize SpotLight based on color , point  and vector. 
 	 * 
 	 * @param intensity is the intensity of the light.
-	 * @param dir - the position for SpotLight.
+	 * @param p0 - the position for SpotLight.
 	 * @param dir - the direction vector SpotLight. 
 	 */
 	public SpotLight(Color intensity, Point p0, Vector dir) {
 		super(intensity, p0);
 		direction = dir.normalize();
+		range = 180;
+	}
+	
+	/**
+	 * Constructor to initialize SpotLight based on color , point  and vector. 
+	 * 
+	 * @param intensity is the intensity of the light.
+	 * @param p0 - the position for SpotLight.
+	 * @param dir - the direction vector SpotLight. 
+	 * @param range - the range of the SpotLight. 
+	 */
+	public SpotLight(Color intensity, Point p0, Vector dir, double range) {
+		super(intensity, p0);
+		direction = dir.normalize();
+		this.range = range;
 	}
 	
 	@Override
