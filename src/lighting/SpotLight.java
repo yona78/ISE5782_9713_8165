2b20458f64c38f0f92423a03d7e34c154cfdc9da
yona78 <yonaorunov1@gmail.com>
2022-04-11 16:05:44 +0300

    Targil7 -done

--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -9,6 +9,7 @@ import primitives.*;
  */
 public class SpotLight extends PointLight{
 	private Vector direction;
+	private double range;
 	/**
 	 * Constructor to initialize SpotLight based on color , point  and vector. 
 	 * 
@@ -19,8 +20,22 @@ public class SpotLight extends PointLight{
 	public SpotLight(Color intensity, Point p0, Vector dir) {
 		super(intensity, p0);
 		direction = dir.normalize();
+		range = 180;
 	}
 	
+	/**
+	 * Constructor to initialize SpotLight based on color , point  and vector. 
+	 * 
+	 * @param intensity is the intensity of the light.
+	 * @param p0 - the position for SpotLight.
+	 * @param dir - the direction vector SpotLight. 
+	 * @param range - the range of the SpotLight. 
+	 */
+	public SpotLight(Color intensity, Point p0, Vector dir, double range) {
+		super(intensity, p0);
+		direction = dir.normalize();
+		this.range = range;
+	}
 	
 	@Override
     public Color getIntensity(Point p) {
