fa0576e05a3e8f294e198701d9f62ff39e0be05d
הילל קרויטורו <1hilell2@gmail.com>
2022-05-01 14:22:23 +0300

    remove setVPSize

--- src/unittests/renderer/CameraTests.java
+++ src/unittests/renderer/CameraTests.java
@@ -35,15 +35,15 @@ class CameraTests {
 				badRay);
 
 		// BV02: 3X3 Center of Upper Side (0,1)
-		assertEquals(new Ray(ZERO_POINT, new Vector(0, -2, -10)), camera.setVPSize(6, 6).constructRay(3, 3, 1, 0),
+		assertEquals(new Ray(ZERO_POINT, new Vector(0, -2, -10)), camera.constructRay(3, 3, 1, 0),
 				badRay);
 
 		// BV03: 3X3 Center of Left Side (1,0)
-		assertEquals(new Ray(ZERO_POINT, new Vector(2, 0, -10)), camera.setVPSize(6, 6).constructRay(3, 3, 0, 1),
+		assertEquals(new Ray(ZERO_POINT, new Vector(2, 0, -10)), camera.constructRay(3, 3, 0, 1),
 				badRay);
 
 		// BV04: 3X3 Corner (0,0)
-		assertEquals(new Ray(ZERO_POINT, new Vector(2, -2, -10)), camera.setVPSize(6, 6).constructRay(3, 3, 0, 0),
+		assertEquals(new Ray(ZERO_POINT, new Vector(2, -2, -10)), camera.constructRay(3, 3, 0, 0),
 				badRay);
 
 		// BV05: 4X4 Corner (0,0)
@@ -51,7 +51,7 @@ class CameraTests {
 				badRay);
 
 		// BV06: 4X4 Side (0,1)
-		assertEquals(new Ray(ZERO_POINT, new Vector(1, -3, -10)), camera.setVPSize(8, 8).constructRay(4, 4, 1, 0),
+		assertEquals(new Ray(ZERO_POINT, new Vector(1, -3, -10)), camera.constructRay(4, 4, 1, 0),
 				badRay);
 
 	}
