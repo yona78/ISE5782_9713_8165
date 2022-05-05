359b492034cd8345b81c5c7cfd7266f09ab8c5c8
הילל קרויטורו <1hilell2@gmail.com>
2022-05-05 10:40:38 +0300

    constructMultipleRefractedRays

--- src/renderer/CastMultipleRays.java
+++ src/renderer/CastMultipleRays.java
@@ -5,7 +5,7 @@ import java.util.List;
 import primitives.*;
 
 public class CastMultipleRays {
-	public static List<Ray> constructMultipleReflectedRay(Point p, Vector v, Vector n, int c) {
+	public static List<Ray> constructMultipleReflectedRays(Point p, Vector v, Vector n, int c) {
 		Vector r = v.subtract(n.scale(2 * v.dotProduct(n)));
 		List<Ray> l = List.of(new Ray(p, r, n));
 		if (c != 1) {
@@ -13,6 +13,21 @@ public class CastMultipleRays {
 			Vector minusVn = vn.scale(-1);
 			for (int i = 1; i < c; ++i) {
 				r = v.subtract(n.scale((4 + (double) i / c) * v.dotProduct(n) * (i % 2 - 0.5)));
+				r.add(((i %4 == 1?vn:minusVn)));
+				l.add(new Ray(p, r, n));
+			}
+		}
+		return l;
+	}
+	
+	public static List<Ray> constructMultipleRefractedRays(Point p, Vector v, Vector n, int c) {
+		Vector r = v;
+		List<Ray> l = List.of(new Ray(p, r, n));
+		if (c != 1) {
+			Vector vn = v.crossProduct(n).normalize();
+			Vector minusVn = vn.scale(-1);
+			for (int i = 1; i < c; ++i) {
+				r = v;//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 				r.add(((i %4 == 1?vn:minusVn)));
 				l.add(new Ray(p, r, n));
 			}
