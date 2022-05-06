eccb5ee0e814536b33a75ab5df5e6f272a741898
הילל קרויטורו <1hilell2@gmail.com>
2022-05-06 16:55:04 +0300

    s

--- src/renderer/CastMultipleRays.java
+++ src/renderer/CastMultipleRays.java
@@ -3,12 +3,38 @@ package renderer;
 import java.util.List;
 import primitives.*;
 
+interface Func {
+    Vector func(Vector v, Vector n, int i, int c);
+}
+
 /**
  * This class helps RayTracerBasic to create multiple rays
  * 
  * @author Hillel Kroitoro, Yona Orunov
  */
 public class CastMultipleRays {
+	/**
+	 * The function calculates the new reflected rays.
+	 * 
+	 * @param p - is the intersection point between the ray and the geometry.
+	 * @param v - is the direction vector of the ray.
+	 * @param n - is the normal vector to the geometry.
+	 * @param c - is the amount of reflected rays.
+	 * @return list of the reflected rays.
+	 */
+	public static List<Ray> constructMultipleRays(Point p, Vector v, Vector n, Ray mainRay, int c, Func f) {
+		List<Ray> l = List.of(mainRay);
+		Vector r;
+		Vector vn = v.crossProduct(n).normalize();
+		Vector minusVn = vn.scale(-1);
+		for (int i = 1; i < c; ++i) {
+			r = f.func(vn, minusVn, i, c);
+			r.add(((i % 4 == 1 ? vn : minusVn)));
+			l.add(new Ray(p, r, n));
+		}
+		return l;
+	}
+	
 	/**
 	 * The function calculates the new reflected rays.
 	 * 
