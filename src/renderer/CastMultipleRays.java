ba167bb176ebb3e780c7866156130bdb3b887303
הילל קרויטורו <1hilell2@gmail.com>
2022-05-06 14:17:13 +0300

    Merge branch 'main' of https://github.com/TFMHK/ISE5782_9713_8165.git into main

--- src/renderer/CastMultipleRays.java
+++ src/renderer/CastMultipleRays.java
@@ -18,21 +18,19 @@ public class CastMultipleRays {
 	 * @param c - is the amount of reflected rays.
 	 * @return list of the reflected rays.
 	 */
-	public static List<Ray> constructMultipleReflectedRays(Point p, Vector v, Vector n, int c) {
-		Vector r = v.subtract(n.scale(2 * v.dotProduct(n)));
-		List<Ray> l = List.of(new Ray(p, r, n));
-		if (c != 1) {
-			Vector vn = v.crossProduct(n).normalize();
-			Vector minusVn = vn.scale(-1);
-			for (int i = 1; i < c; ++i) {
-				r = v.subtract(n.scale((4 + (double) i / c) * v.dotProduct(n) * (i % 2 - 0.5)));
-				r.add(((i %4 == 1?vn:minusVn)));
-				l.add(new Ray(p, r, n));
-			}
+	public static List<Ray> constructMultipleReflectedRays(Point p, Vector v, Vector n, Ray mainRay, int c) {
+		List<Ray> l = List.of(mainRay);
+		Vector r;
+		Vector vn = v.crossProduct(n).normalize();
+		Vector minusVn = vn.scale(-1);
+		for (int i = 1; i < c; ++i) {
+			r = v.subtract(n.scale((4 + (double) i / c) * v.dotProduct(n) * (i % 2 - 0.5)));
+			r.add(((i % 4 == 1 ? vn : minusVn)));
+			l.add(new Ray(p, r, n));
 		}
 		return l;
 	}
-	
+
 	/**
 	 * The function calculates the new refracted ray.
 	 * 
@@ -49,8 +47,8 @@ public class CastMultipleRays {
 			Vector vn = v.crossProduct(n).normalize();
 			Vector minusVn = vn.scale(-1);
 			for (int i = 1; i < c; ++i) {
-				r = v;//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-				r.add(((i %4 == 1?vn:minusVn)));
+				r = v;// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+				r.add(((i % 4 == 1 ? vn : minusVn)));
 				l.add(new Ray(p, r, n));
 			}
 		}
