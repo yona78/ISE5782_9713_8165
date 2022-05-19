800d476119dc81b77dc7ce6f3c1904be51642dec
הילל קרויטורו <1hilell2@gmail.com>
2022-05-19 15:46:26 +0300

    Box

---
+++ src/geometries/Box.java
@@ -0,0 +1,18 @@
+package geometries;
+
+import primitives.Point;
+import primitives.Vector;
+
+public class Box extends Polygon{
+	public Box(Geometry g, Point p, Vector minusVto, Vector vRight, Vector vUp) {
+		int minX = Integer.MAX_VALUE;
+		int minY = Integer.MAX_VALUE;
+		int minZ = Integer.MAX_VALUE;
+		
+		int maxX = Integer.MIN_VALUE;
+		int maxY = Integer.MIN_VALUE;
+		int maxZ = Integer.MIN_VALUE;
+		
+		if()
+	}
+}
